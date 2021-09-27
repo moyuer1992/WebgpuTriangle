75a946440b01441ce6a044eec6e37a82292ab88e
xuepei <xuepei92@gmail.com>
2021-09-27 10:20:57 +0800

    webgpu triangle

---
+++ src/index.ts
@@ -0,0 +1,114 @@
+const vertexShaderSource: string = `
+  [[stage(vertex)]]
+  fn main([[builtin(vertex_index)]] VertexIndex : u32)
+       -> [[builtin(position)]] vec4<f32> {
+    var pos = array<vec2<f32>, 3>(
+        vec2<f32>(0.0, 0.5),
+        vec2<f32>(-0.5, -0.5),
+        vec2<f32>(0.5, -0.5));
+
+    return vec4<f32>(pos[VertexIndex], 0.0, 1.0);
+  }
+`
+
+const fragShaderSource: string = `
+  [[stage(fragment)]]
+  fn main() -> [[location(0)]] vec4<f32> {
+    return vec4<f32>(1.0, 0.0, 0.0, 1.0);
+  }
+`
+
+const init = async (canvas: HTMLCanvasElement) => {
+  const adapter = await navigator.gpu.requestAdapter() as GPUAdapter
+  const device = await adapter.requestDevice() as GPUDevice
+  const context = canvas.getContext('webgpu') as GPUCanvasContext
+  const devicePixelRatio = window.devicePixelRatio || 1
+  const presentationFormat = context.getPreferredFormat(adapter)
+  const presentationSize = [
+    canvas.clientWidth * devicePixelRatio,
+    canvas.clientHeight * devicePixelRatio,
+  ]
+  context.configure({
+    device,
+    format: presentationFormat,
+    size: presentationSize,
+  })
+  return {
+    adapter,
+    device,
+    context,
+  }
+}
+
+const createPipeline = (
+  vertexShaderSource: string,
+  fragShaderSource: string,
+  adapter: GPUAdapter,
+  device: GPUDevice,
+  context: GPUCanvasContext,
+) => {
+  const presentationFormat = context.getPreferredFormat(adapter)
+  return device.createRenderPipeline({
+    vertex: {
+      module: device.createShaderModule({
+        code: vertexShaderSource,
+      }),
+      entryPoint: 'main',
+    },
+    fragment: {
+      module: device.createShaderModule({
+        code: fragShaderSource,
+      }),
+      entryPoint: 'main',
+      targets: [
+        {
+          format: presentationFormat,
+        },
+      ],
+    },
+    primitive: {
+      topology: 'triangle-list',
+    },
+  })
+}
+
+async function main () {
+  const canvas = document.getElementById('canvas') as HTMLCanvasElement
+  const {
+    adapter,
+    device,
+    context,
+  } = await init(canvas)
+  const pipeline = createPipeline(
+    vertexShaderSource,
+    fragShaderSource,
+    adapter as GPUAdapter,
+    device as GPUDevice,
+    context as GPUCanvasContext,
+  )
+  const frame = () => {
+    const commandEncoder = device.createCommandEncoder()
+    const textureView = context.getCurrentTexture().createView()
+
+    const renderPassDescriptor: GPURenderPassDescriptor = {
+      colorAttachments: [
+        {
+          view: textureView,
+          loadValue: { r: 0.0, g: 0.0, b: 0.0, a: 1.0 },
+          storeOp: 'store',
+        },
+      ],
+    }
+
+    const passEncoder = commandEncoder.beginRenderPass(renderPassDescriptor)
+    passEncoder.setPipeline(pipeline)
+    passEncoder.draw(3, 1, 0, 0)
+    passEncoder.endPass()
+
+    device.queue.submit([commandEncoder.finish()])
+    requestAnimationFrame(frame)
+  }
+  requestAnimationFrame(frame)
+}
+
+main()
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.ts',
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.wgsl$/i,
+        use: 'raw-loader',
+      }
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+};
